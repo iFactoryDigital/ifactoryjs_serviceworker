0a4657abd2f6b7e6fbf51b077e7502e4a94b5ba2
EdenCoder <alex@adlist.co.nz>
2018-11-01 16:25:35 +1000

    add serviceworker controller

---
+++ bundles/serviceworker/controllers/serviceworker.js
@@ -0,0 +1,26 @@
+
+// Require local class dependencies
+const Controller = require('controller');
+
+/**
+ * Create Core Controller class
+ */
+class ServiceworkerController extends Controller {
+
+  /**
+   * Construct Home Controller class
+   */
+  constructor () {
+    // Run super
+    super();
+    
+  }
+
+}
+
+/**
+ * Export Core Controller class
+ *
+ * @type {ServiceworkerController}
+ */
+exports = module.exports = ServiceworkerController;
